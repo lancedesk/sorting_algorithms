f5e298b1a397091c8d7a771cb9bf20d4875f5911
Lance Desk <lncedesk@gmail.com>
2023-11-21 10:49:13 -0800

    A function that sorts an array of integers in ascending order using the Radix sort algorithm

--- sort.h
+++ sort.h
@@ -31,7 +31,7 @@ typedef struct listint_s
 } listint_t;
 
 
-
+void radix_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
